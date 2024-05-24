793db04e29f30effd19f38b6605a3037c4121bb5
Fardeen <fardeenzubair@gmail.com>
2024-05-24 15:57:19 +0530

    database

--- server/config/database.js
+++ server/config/database.js
@@ -10,6 +10,16 @@ if (process.env.DB_URL) {
     dialect: 'postgres',
     ssl: true,
   });
+
+   sequelize.authenticate()
+    .then(() => {
+      console.log('Database connection has been established successfully.');
+    })
+    .catch(err => {
+      console.error('Unable to connect to the database:', err);
+      process.exit(1); // Exit the application if the connection fails
+    });
+    
 } else {
   console.error("DB_URL is not available. Please check your environment configuration.");
   process.exit(1); // Exit the application if DB_URL is not available
